0d64e02b66b480721e9404c5a4d91f17e51bce33
Wayrureetoburin <s6406021621111@email.kmutnb.ac.th>
2022-12-29 10:15:35 +0700

    update

---
+++ Lecture2/Ex-01.js
@@ -0,0 +1,7 @@
+function findPerimeter(width,Lenght) {
+    return 2*(width + Lenght);
+    
+}
+console.log (findPerimeter(6, 7));
+console.log (findPerimeter(20, 10));
+console.log (findPerimeter(2, 9));
--- Lecture2/strsplit.js
+++ Lecture2/strsplit.js
@@ -2,7 +2,7 @@ const str = 'The quick brown fox jumps over the lazy dog.';
 
 const words = str.split(' ');
 console.log(words[3]);
-// expexted output: "fox"
+// expected output: "fox"
 
 const chars = str.split('');
 console.log(chars[8]);
