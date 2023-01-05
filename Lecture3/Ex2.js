0ad8d2de38777a5f221e3aa311b303974f5a437a
Wayrureetoburin <s6406021621111@email.kmutnb.ac.th>
2023-01-05 11:39:53 +0700

    update

---
+++ Lecture3/Ex2.js
@@ -0,0 +1,11 @@
+for (let char of "Table") {
+    console.log( char ); 
+}
+console.log();
+for (let char of "Chairs") {
+    console.log( char ); 
+}
+console.log();
+for (let char of "Couch") {
+    console.log( char ); 
+}
--- Lecture3/maparray.js
+++ Lecture3/maparray.js
@@ -1,7 +1,7 @@
 const finalParticipants = ["Taylor", "Donald", "Don", "Natasha", "Bobby"];
 
 const announcements = finalParticipants.map((member) => {
-    return member + "joined the contest.";
+    return member + " joined the contest.";
 });
 
 console.log(announcements);
